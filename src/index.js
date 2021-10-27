714e854b8f7e1e93c86cc1604e1e6acbf1bc037d
airmanxtw <airmanx@stust.edu.tw>
2021-10-27 23:45:47 +0800

    demo axios

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 
 import Vue from "vue";
 import root from "./components/root.vue";
-import axios from "axios";
-import VueAxios from "vue-axios";
-
-Vue.use(VueAxios,axios);
 
 new Vue({
     el:'#app',
