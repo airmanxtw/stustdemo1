ce50006086a4bdedce6d0a88196b8497928f858f
airmanxtw <airmanx@stust.edu.tw>
2021-10-28 21:01:01 +0800

    search demo

--- src/components/newsSearch.vue
+++ src/components/newsSearch.vue
@@ -1,30 +1,32 @@
 <template>
-    <div>
-        <span>關鍵字：</span><input v-model="keyword"/>
-        <select>
-            <option :key="s.id" v-for="s in searchResult">{{s.title}}</option>
-        </select>
-    </div>
+  <div>
+    <span>關鍵字：</span><input v-model="keyword" />
+    <select>
+      <option :key="s.id" v-for="s in searchResult">{{ s.title }}</option>
+    </select>
+  </div>
 </template>
 <script>
 export default {
-    data() {
-        return {
-            keyword:'',
-            searchResult:[]
-        }
+  data() {
+    return {
+      keyword: "",
+      searchResult: [],
+    };
+  },
+  methods: {
+    search() {
+      this.axios
+        .get(`https://portal.stust.edu.tw/exteduapi/data/news/keyword/${this.keyword}`)
+        .then((res) => {
+          this.searchResult = res.data;
+        });
     },
-    methods: {
-        search(){
-            this.axios.get(`https://portal.stust.edu.tw/exteduapi/data/news/keyword/${this.keyword}`).then(res=>{
-                this.searchResult=res.data;
-            })
-        }
+  },
+  watch: {
+    keyword() {
+      this.search();
     },
-    watch: {
-        keyword(){
-            this.search();
-        }
-    },
-}
+  },
+};
 </script>
