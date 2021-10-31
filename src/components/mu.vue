90f3f06a7b01118a1991656e4c93cabe6ebe1e39
airmanxtw <airmanx@stust.edu.tw>
2021-10-31 21:09:55 +0800

    11/1

--- src/components/mu.vue
+++ src/components/mu.vue
@@ -1,10 +1,15 @@
 // * 九九乘法表 by vue
 <template>
-  <tbody>
-    <tr v-for="i in [2, 6]">
-      <td v-for="n in 4">
-        <div v-for="m in 9">{{ n + i - 1 }}x{{ m }}={{ (n + i - 1) * m }}</div>
-      </td>
-    </tr>
-  </tbody>
+  <table>
+    <tbody>
+      <tr v-for="i in [2, 6]">
+        <td v-for="n in 4">
+          <div v-for="m in 9">
+            {{ n + i - 1 }}x{{ m }}={{ (n + i - 1) * m }}</button>
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
+
