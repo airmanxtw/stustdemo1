7e7034d539ad800b6d4f023375e6af40b4a822fd
airmanxtw <airmanx@stust.edu.tw>
2021-11-01 10:05:44 +0800

    1101

--- src/components/root.vue
+++ src/components/root.vue
@@ -1,13 +1,43 @@
 <template>
-  <table>
-    <mu />
-  </table>
+  <div>
+    <!-- -{{text}}- -->
+    <!-- <br/> -->
+    <!-- <input :value="text" @input="text=$event.target.value" /> -->
+    <!-- <input v-model="text"/> -->
+    <studList :studs="rootstuds" @delete="rootdelete" />
+
+    <div>
+      <span>學號：<input v-model="newstuds.no" /></span>
+      <span>姓名：<input v-model="newstuds.name" /></span>
+      <button @click="rootstuds.push({ no: newstuds.no, name: newstuds.name })">
+        新增
+      </button>
+    </div>
+  </div>
 </template>
 <script>
-import mu from "./mu";
+import studList from "./studList.vue";
 export default {
+  data() {
+    return {
+      text: "abcd",
+      newstuds: {
+        no: "",
+        name: "",
+      },
+      rootstuds: [
+        { no: "12345", name: "airmanx" },
+        { no: "54321", name: "ccsu" },
+      ],
+    };
+  },
+  methods: {
+    rootdelete(studno) {
+      this.rootstuds = this.rootstuds.filter((item) => item.no != studno);
+    },
+  },
   components: {
-    mu,
+    studList,
   },
 };
 </script>
---
+++ src/components/studList.vue
@@ -0,0 +1,34 @@
+<template>
+    <table>
+        <tr>
+            <th>學號</th>
+            <th>姓名</th>
+            <th>刪除</th>
+        </tr>
+        <tr>
+            <td colspan="3">{{`筆數：${size}`}}</td>
+        </tr>
+        <tr :key="stud.no" v-for="stud in studs">
+            <td>{{stud.no}}</td>
+            <td>{{stud.name}}</td>
+            <td>
+                <button v-on:click="$emit('delete',stud.no)">刪除</button>
+            </td>
+        </tr>
+    </table>
+</template>
+<script>
+export default {
+    props:{
+        studs:{
+            type:Array,
+            default:[]
+        }
+    },
+    computed: {
+        size(){
+            return this.studs.length;
+        }
+    },
+}
+</script>
