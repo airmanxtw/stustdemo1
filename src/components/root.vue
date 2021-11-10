ba8364a384a46c37d0724f31c315b87a9e3ecdcf
airmanxtw <airmanx@stust.edu.tw>
2021-11-10 08:49:44 +0800

    test ...

--- src/components/root.vue
+++ src/components/root.vue
@@ -21,5 +21,71 @@ export default {
   components: {
     addfileds,
   },
+  created() {
+    let students = [
+      {
+        studno: "A12345",
+        studname: "顏大鈞",
+        classno: "A10",
+        classna: "資工一甲",
+      },
+      {
+        studno: "A12346",
+        studname: "施志鴻",
+        score: 60,
+      },
+    ];
+
+    let s = {
+      studno: "A12345",
+      studname: "顏大鈞",
+      classno: "A10",
+      classna: "資工一甲",
+    };
+
+    // studnet={...student};
+
+    // students = [...stud];
+
+    // students = students.map((student) => ({
+    //   ...{ score: null, classno: null, classna: null },
+    //   ...student,
+    // }));
+
+    debugger;
+
+    let student = {
+      studno: "A12345",
+      studname: "顏大鈞",
+      score: null,
+    };
+
+    student={...student,...{age:10},...{classno:"A123"}};
+
+    let a1=[1,2,3];
+    let b1=[4,5,6];
+
+    let c=[...a1,...b1];
+
+    
+
+    studnet = { ...student, ...{ classno: "A10", classna: "資工一甲" } };
+
+    student.classno = "A10";
+    student.classna = "資工一甲";
+
+    let ans1 = student.score;
+    let ans2 = student.classno;
+
+    if (student.classno === undefined) undefined;
+
+    let a = [{ a: 10, b: 20 }, { a: 20 }];
+    let b = [].push({ b: null });
+    let y = a.map((item) => {
+      return { ...{ b: null }, ...item };
+    });
+    let x = { ...{ b: null }, ...{ a: 10 } };
+    debugger;
+  },
 };
 </script>
