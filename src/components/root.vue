faf445f26d1272c0ab4ec6ca1583a46e32ea2ddf
da-chun yen <airmanx.tw@yahoo.com.tw>
2021-11-16 14:11:03 +0800

    test

--- src/components/root.vue
+++ src/components/root.vue
@@ -2,26 +2,23 @@
   <v-app>
     <v-main>
       <v-container>
-      <h2>Root</h2>
-      <router-link to="/">to home</router-link>
-      <router-link to="/a">to a</router-link>
-      <router-link to="/b">to b</router-link>
-      <router-link to="/c">to c</router-link>
-      <!-- <button @click='goa'>go a</button> -->
-      <router-view></router-view>
-      <addfileds />
-      
-    <v-btn color="pink lighten-5" @click="goa">
-      <v-icon>mdi-account</v-icon>
-      新增
-      </v-btn>
-      name:{{name}}
-      <myinput v-model="name" />
+        <h2>Root</h2>
+        <router-link to="/">to home</router-link>
+        <router-link to="/a">to a</router-link>
+        <router-link to="/b">to b</router-link>
+        <router-link to="/c">to c</router-link>
+        <!-- <button @click='goa'>go a</button> -->
+        <router-view></router-view>
+        <addfileds />
 
+        <v-btn color="pink lighten-5" @click="goa">
+          <v-icon>mdi-account</v-icon>新增</v-btn
+        >name:{{ name }}
+        <myinput v-model="name" />
 
-      <!-- <myinput :value="name" @change="(val)=>{name=val}" /> -->
-      <!-- <myinput :value.sync="name" /> -->
-    </v-container>
+        <!-- <myinput :value="name" @change="(val)=>{name=val}" /> -->
+        <!-- <myinput :value.sync="name" /> -->
+      </v-container>
     </v-main>
   </v-app>
 </template>
@@ -31,8 +28,8 @@ import myinput from "./exams/myinput/myinput";
 export default {
   data() {
     return {
-      name:''
-    }
+      name: "",
+    };
   },
   methods: {
     goa() {
@@ -40,7 +37,8 @@ export default {
     },
   },
   components: {
-    addfileds,myinput
+    addfileds,
+    myinput,
   },
   created() {},
 };
