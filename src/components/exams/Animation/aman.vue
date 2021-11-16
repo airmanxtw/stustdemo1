3017f49a121da69735ee254cc3936fcc20b7f7c8
airmanxtw <airmanx@stust.edu.tw>
2021-11-16 16:07:25 +0800

    aman

---
+++ src/components/exams/Animation/aman.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-icon large>mdi-{{icons[counter]}}</v-icon>
+</template>
+<script>
+export default {
+    props:{
+        delay:{
+            type:Number,
+            default:500
+        } 
+    },
+    data() {
+        return {
+            icons:["human-handsup","human-handsdown"],
+            counter:0
+        }
+    },
+    created() {
+        setInterval(()=>{
+            this.counter=(this.counter + 1) % 2;
+        },this.delay);
+    },
+
+}
+</script>
--- src/components/root.vue
+++ src/components/root.vue
@@ -7,39 +7,17 @@
         <router-link to="/a">to a</router-link>
         <router-link to="/b">to b</router-link>
         <router-link to="/c">to c</router-link>
-        <!-- <button @click='goa'>go a</button> -->
         <router-view></router-view>
-        <addfileds />
-
-        <v-btn color="pink lighten-5" @click="goa">
-          <v-icon>mdi-account</v-icon>新增</v-btn
-        >name:{{ name }}
-        <myinput v-model="name" />
-
-        <!-- <myinput :value="name" @change="(val)=>{name=val}" /> -->
-        <!-- <myinput :value.sync="name" /> -->
+        <aman/>
       </v-container>
     </v-main>
   </v-app>
 </template>
 <script>
-import addfileds from "./exams/addFiled/addFiled.vue";
-import myinput from "./exams/myinput/myinput";
+import aman from "./exams/Animation/aman.vue";
 export default {
-  data() {
-    return {
-      name: "",
-    };
-  },
-  methods: {
-    goa() {
-      this.$router.push("a");
-    },
-  },
-  components: {
-    addfileds,
-    myinput,
-  },
-  created() {},
+  components:{
+    aman
+  }
 };
 </script>
