84c9a96dc6f7750228a3159192efd3d1c031b19a
da-chun yen <airmanx.tw@yahoo.com.tw>
2021-11-26 14:12:31 +0800

    test

--- src/components/root.vue
+++ src/components/root.vue
@@ -3,12 +3,12 @@
     <v-main>
       <v-container>
         <h2>Root2</h2>
-        <sign/>
+        <!-- <sign/>
         <router-link to="/">to home</router-link>
         <router-link to="/a">to a</router-link>
         <router-link to="/b">to b</router-link>
         <router-link to="/c">to c</router-link>
-        <router-view></router-view>
+        <router-view></router-view> -->
         <v-btn @click="dialog=true"><v-icon>mdi-lead-pencil</v-icon>新增學生</v-btn>
 
         <v-dialog v-model="dialog" :max-width="400">
@@ -21,9 +21,8 @@
 </template>
 
 <script>
-import Sign from './exams/dialogDemo/sign.vue';
+
 import StudForm from './exams/form/studForm.vue';
-import Image2 from './exams/imageps/image2.vue';
 
 export default {
   data() {
@@ -32,9 +31,6 @@ export default {
     }
   },
   components:{
-    Image2,
-
-    Sign,
     StudForm,
   }
 }
