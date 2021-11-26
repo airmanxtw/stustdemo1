e3b53b9c5531dd1e8aed8dfb45e58a6825e6e58c
da-chun yen <airmanx.tw@yahoo.com.tw>
2021-11-26 15:19:11 +0800

    test

--- src/components/root.vue
+++ src/components/root.vue
@@ -9,29 +9,31 @@
         <router-link to="/b">to b</router-link>
         <router-link to="/c">to c</router-link>
         <router-view></router-view> -->
-        <v-btn @click="dialog=true"><v-icon>mdi-lead-pencil</v-icon>新增學生</v-btn>
+        <v-btn @click="dialog = true" :loading="true"
+          ><v-icon>mdi-lead-pencil</v-icon>
+          <template v-slot:loader>我好忙啊!</template>
+          新增學生
+        </v-btn>
 
         <v-dialog v-model="dialog" :max-width="400">
-          <stud-form @inserted="dialog=false"></stud-form>
+          <stud-form @inserted="dialog = false"></stud-form>
         </v-dialog>
-        
       </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-
-import StudForm from './exams/form/studForm.vue';
+import StudForm from "./exams/form/studForm.vue";
 
 export default {
   data() {
     return {
-      dialog:false
-    }
+      dialog: false,
+    };
   },
-  components:{
+  components: {
     StudForm,
-  }
-}
+  },
+};
 </script>
