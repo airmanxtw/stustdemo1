93c283b5edf5d53b6c4d34e6a0b95e29b4b422e1
airmanxtw <airmanx@stust.edu.tw>
2021-12-16 11:22:24 +0800

    props example

--- src/components/exams/myinput/myinput.vue
+++ src/components/exams/myinput/myinput.vue
@@ -1,27 +1,36 @@
 <template>
-    <v-text-field :value="value" @change="mychange($event)"></v-text-field>
+  <div>
+    <v-text-field :value="value" v-bind="textProps"></v-text-field>
+    <v-btn v-bind="btnProps">確認</v-btn>
+  </div>
 </template>
 <script>
 export default {
-    model:{
-        prop:"value",
-        event:"change"
+  model: {
+    prop: "value",
+    event: "change",
+  },
+  data() {
+    return {};
+  },
+  props: {
+    value: {
+      type: String,
+      default: "",
     },
-    data() {
-        return {
-            
-        }
+    textProps: {
+      type: Object,
+      default: null,
     },
-    props:{
-        value:{
-            type:String,
-            default:''
-        }
+    btnProps: {
+      type: Object,
+      default: null,
     },
-    methods: {
-        mychange(event){
-            this.$emit('change',event);            
-        }
+  },
+  methods: {
+    mychange(event) {
+      this.$emit("change", event);
     },
-}
+  },
+};
 </script>
--- src/components/root.vue
+++ src/components/root.vue
@@ -9,11 +9,17 @@
         <router-link to="/b">to b</router-link>
         <router-link to="/c">to c</router-link>
         <router-view></router-view>
-        
+
         <!-- <p v-html="constructor.constructor('alert(1)')()"></p> -->
-        <injection-1/>
+        <injection-1 />
         <h1>key</h1>
-       
+
+        <myinput
+          :textProps="{ outlined:true, dense: true }"
+          :btnProps="{ color: 'primary', large: true }"
+        ></myinput>
+
+
       </v-container>
     </v-main>
   </v-app>
@@ -21,7 +27,8 @@
 
 <script>
 import StudForm from "./exams/form/studForm.vue";
-import Injection1 from './exams/injection/injection1.vue';
+import Injection1 from "./exams/injection/injection1.vue";
+import Myinput from "./exams/myinput/myinput.vue";
 import Socket from "./exams/socket/socket.vue";
 
 export default {
@@ -29,13 +36,14 @@ export default {
     return {
       dialog: false,
       id: 1,
-      inject:"constructor.constructor('alert(1)')()"
+      inject: "constructor.constructor('alert(1)')()",
     };
   },
   components: {
     StudForm,
     Socket,
     Injection1,
+    Myinput,
   },
 };
 </script>
